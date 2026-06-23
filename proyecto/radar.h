89598db5b5352c07d864ca3fbc6b87292f1cfadb
LuisGargate06 <amirgargatecollantes@gmail.com>
2026-06-23 13:03:18 -0500

    segundo avance

--- proyecto/radar.h
+++ proyecto/radar.h
@@ -32,4 +32,39 @@ struct vehiculo {
             }
             *(placa + 7) = '\0';
         }
+
+        //Declaramos las funciones
+        //Nos acordamos que la formula para calcular la velocidad es variación de la posición sobre la variación del tiempo
+        void calcularVelocidad(const T* deltaX, const T* deltaT, T* velocidadResultado);
+        void capturarTraficoSimulado(vehiculo<T> listaAutos[], T1* cantidadDetectados);
+};
+
+//Definimos las funciones
+//Calculamos la velocidad de cada vehículo
+template <typename T>
+void vehiculo<T>::calcularVelocidad(const T* deltaX, const T* deltaT, T* velocidadResultado){
+    if(deltaT != nullptr && *deltaT > 0.0f){ //el denominador no debe ser 0, por teoria
+        *velocidadResultado = (*deltaX)/(*deltaT);
+    }
+    else{
+        *velocidadResultado = 0;
+    }
+}
+
+//Simulador de trafico para llenar el arreglo unidimensional listaAutos
+template <typename T>
+void vehiculo<T>:: capturarTraficoSimulado(vehiculo<T> listaAutos[], int* cantidadDetectados){
+    //Inicializamos 
+    *cantidadDetectados = 0;
+
+    if(*cantidadDetectados < max_vehiculos){
+        T dX = 20.0; //metros
+        T dT = 1.0; //segundos
+        T velocidadCalculada;
+
+        //Llamamos a la funcion calcularVelocidad
+        calcularVelocidad(&dX, &dT, &velocidadCalculada);
+
+        listaAutos[*cantidadDetectados].
+    }
 }
