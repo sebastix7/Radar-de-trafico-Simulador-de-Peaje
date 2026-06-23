7b175152df2322d0c238c9a5e887547fcdd1d328
LuisGargate06 <amirgargatecollantes@gmail.com>
2026-06-23 18:25:43 -0500

    Tercer avance

--- proyecto/radar.h
+++ proyecto/radar.h
@@ -1,7 +1,8 @@
 #ifndef RADAR_H
 #define RADAR_H
 #include "types1.h"
-#include <cstddef>
+#include <cstddef> //Para el size_t
+#include <fstream> // Para el ifstream
 
 //Suponemos que el hardware tiene un alcance de 50 metros 
 //Por ende en un carril tiene como capacidad máxima de 12 vehículos
@@ -51,20 +52,54 @@ void vehiculo<T>::calcularVelocidad(const T* deltaX, const T* deltaT, T* velocid
     }
 }
 
-//Simulador de trafico para llenar el arreglo unidimensional listaAutos
+//Al no tener un sensor para saber la distancia y tiempo recorrido de los autos 
+//Hacemos que esta función lea un archivo el cual ya contiene los datos de cada auto
 template <typename T>
-void vehiculo<T>:: capturarTraficoSimulado(vehiculo<T> listaAutos[], int* cantidadDetectados){
-    //Inicializamos 
+void vehiculo<T>:: capturarTraficoSimulado(vehiculo<T> listaAutos[], T1* cantidadDetectados){
+    //Inicializamos
     *cantidadDetectados = 0;
 
-    if(*cantidadDetectados < max_vehiculos){
-        T dX = 20.0; //metros
-        T dT = 1.0; //segundos
-        T velocidadCalculada;
+    //Abrimos el archivo de texto
+    std::ifstream archivo("trafico.txt");
 
-        //Llamamos a la funcion calcularVelocidad
-        calcularVelocidad(&dX, &dT, &velocidadCalculada);
-
-        listaAutos[*cantidadDetectados].
+    //Nos fijamos si el archivo existe
+    if(!archivo.is_open()){
+        return;
     }
+
+    //Inicializamos el contador del total de autos en el archivo
+    T1 totalAutosEnArchivo = 0;
+    archivo >> totalAutosEnArchivo; //Leemos la primera linea
+
+    //Creamos variables para absorver los datos de cada fila
+    T1 temp_id;
+    T temp_dX;
+    T temp_dT;
+    char temp_placa[8];
+    T velocidadCalculada;
+
+    //Creamos un bucle para leer cada fila
+    while(*cantidadDetectados < max_vehiculos && *cantidadDetectados < totalAutosEnArchivo){
+        //Leemos 
+        //Usamos el if como seguridad para fijarnos si aun se pueden leer valores
+        if(archivo >> temp_id >> temp_dX >> temp_dT >> temp_placa){
+            //Calculamos la velocidad del auto
+            calcularVelocidad(&temp_dX, &temp_dT, &velocidadCalculada);
+
+            //Guardamos los datos
+            listaAutos[*cantidadDetectados].llenarDatos(temp_id, velocidadCalculada, temp_placa);
+
+            //Incrementamos
+            (*cantidadDetectados)++;
+        }
+        
+        else{
+            break;
+        }
+    }
+
+    //Cerramos el archivpo
+    archivo.close();
 }
+
+#endif 
